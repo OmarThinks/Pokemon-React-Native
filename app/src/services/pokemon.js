3df829a5c8c205c40a9de55f2dd96d8137566354
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-11-12 16:23:34 +0200

    Added fetching pokemon by name and id

--- app/src/services/pokemon.js
+++ app/src/services/pokemon.js
@@ -8,9 +8,24 @@ export const pokemonApi = createApi({
     getPokemonByName: builder.query({
       query: (name) => `pokemon/${name}`,
     }),
+    getPokemonsPaginator: builder.query({
+      query: (pageNumber) => {
+        const offset = pageNumber * 20;
+        return `pokemon/?offset=${offset}&limit=20`;
+      },
+    }),
+    getPokemonsById: builder.query({
+      query: (id) => {
+        return `pokemon/${id}/`;
+      },
+    }),
   }),
 });
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetPokemonByNameQuery } = pokemonApi;
+export const {
+  useGetPokemonByNameQuery,
+  useGetPokemonsByIdQuery,
+  useGetPokemonsPaginatorQuery,
+} = pokemonApi;
