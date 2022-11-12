ae2645983adb04c35aa13560770684335db052b6
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-11-12 13:55:48 +0200

    Put App.js inside src for autocomplete

---
+++ app/src/App.js
@@ -0,0 +1,17 @@
+import { View, Text } from "react-native";
+
+// App should be here so that autocomplete works
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Universal React with Expo</Text>
+    </View>
+  );
+}
