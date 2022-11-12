a31e9dc38a4eae1ddee3b0ff0976589e4e7f3e46
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-11-12 14:26:14 +0200

    Surounded App with providers

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,22 @@
 import { View, Text } from "react-native";
+import { store } from "./app/store";
+import { Provider as PaperProvider } from "react-native-paper";
+import { Provider } from "react-redux";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
+    <Provider store={store}>
+      <PaperProvider>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>Universal React with Expo</Text>
+        </View>
+      </PaperProvider>
+    </Provider>
   );
 }
