d36e093b3ed068bb950457803a56873e689b4109
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-11-12 21:43:22 +0200

    Preparing details screen

---
+++ app/src/screens/DetailsScreen.js
@@ -0,0 +1,31 @@
+import { Text } from "react-native-paper";
+import { View } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  buttonsContainer: {
+    display: "flex",
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 20,
+  },
+  button: {
+    width: "35%",
+    padding: 5,
+    backgroundColor: "#f9c2ff",
+  },
+  centererView: { flex: 1, justifyContent: "center", alignItems: "center" },
+});
+
+const DetailsScreen = () => {
+  return (
+    <View>
+      {" "}
+      <Text>Details</Text>
+    </View>
+  );
+};
