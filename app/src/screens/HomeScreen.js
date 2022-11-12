02db9076317281dd1b7bc8ef0964ba3e16d1b544
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-11-12 17:40:33 +0200

    Loaded everything as texts

--- app/src/screens/HomeScreen.js
+++ app/src/screens/HomeScreen.js
@@ -1,8 +1,16 @@
 import { View, Text } from "react-native";
 import { useGetPokemonsPaginatorQuery } from "../services/pokemon";
 import { useState } from "react";
+
+const PokemonsList = ({ data }) => {
+  return data.results.map((pokemon) => (
+    <Text key={pokemon.name}>{pokemon.name}</Text>
+  ));
+};
+
 const HomeScreen = () => {
   const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
 
   const incrementPage = () => {
     setPage(page + 1);
@@ -14,18 +22,22 @@ const HomeScreen = () => {
   const {
     data,
     error,
-    isLoading: isLoading1,
+    isFetching: isLoading1,
   } = useGetPokemonsPaginatorQuery(page);
   console.log(data);
+
+  if (!isLoading1) {
+    console.log(data.results);
+  }
 
   const toRender = error ? (
     <Text>Oh no, there was an error</Text>
   ) : isLoading1 ? (
     <Text>Loading...</Text>
   ) : data ? (
-    <Text>
-      <Text>loaded</Text>
-    </Text>
+    <View>
+      <PokemonsList data={data} />
+    </View>
   ) : null;
 
   return (
