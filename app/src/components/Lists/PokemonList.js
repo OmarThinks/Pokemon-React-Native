b490b19fa4ca81280636e0480cba6e373e756f87
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-11-19 06:23:38 +0200

    Used FlatList instead of Scroll View

--- app/src/components/Lists/PokemonList.js
+++ app/src/components/Lists/PokemonList.js
@@ -1,11 +1,25 @@
-import { StyleSheet, StatusBar } from "react-native";
+import { StyleSheet, StatusBar, FlatList } from "react-native";
 import PokemonCard from "../Cards/PokemonCard";
 
 const PokemonsList = ({ data, navigation }) => {
   const toReturn = data.results.map((pokemon) => (
     <PokemonCard item={pokemon} key={pokemon.name} navigation={navigation} />
   ));
-  return toReturn;
+
+  const renderItem = ({ item }) => (
+    <PokemonCard item={item} key={item.name} navigation={"abc"} />
+  );
+
+  const toReturn2 = (
+    <FlatList
+      data={data.results}
+      renderItem={renderItem}
+      keyExtractor={(pokemon) => pokemon.name}
+    />
+  );
+  console.log(data);
+
+  return toReturn2;
 };
 
 export default PokemonsList;
