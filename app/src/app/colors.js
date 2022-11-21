57771142d43defc6a2cad6a9d469e8d91a43b1f6
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-11-21 08:59:53 +0200

    Added colors

---
+++ app/src/app/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+  backGround: "#E5E5E5",
+  backGroundSurface: "#FFFFFF",
+  primaryTextColor: "#040C22",
+  secondaryTextColor: "#363D4E",
+  active: "#F26333",
+  idle: "#5C616F",
+};
+
+export default colors;
