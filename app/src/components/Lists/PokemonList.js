cb2adf1fdfe99a2691ecf932f5812d7009ac3511
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-11-21 08:48:59 +0200

    Added HeaderCompoenet in Home page

--- app/src/components/Lists/PokemonList.js
+++ app/src/components/Lists/PokemonList.js
@@ -1,16 +1,27 @@
-import { StyleSheet, StatusBar, FlatList, Text } from "react-native";
+import { StyleSheet, StatusBar, FlatList, View } from "react-native";
 import PokemonCard from "../Cards/PokemonCard";
+import { Text } from "react-native-paper";
 
 const PokemonsList = ({ data, bottomPagination }) => {
   const renderItem = ({ item }) => <PokemonCard item={item} key={item.name} />;
 
+  const headerComponent = (
+    <View style={{ marginHorizontal: 16 }}>
+      <Text>Pokemons List</Text>
+      <Text>Paginated list of all pokemons</Text>
+    </View>
+  );
+
   const toReturn = (
-    <FlatList
-      data={data.results}
-      renderItem={renderItem}
-      keyExtractor={(pokemon) => pokemon.name}
-      ListFooterComponent={bottomPagination}
-    />
+    <View>
+      <FlatList
+        data={data.results}
+        renderItem={renderItem}
+        keyExtractor={(pokemon) => pokemon.name}
+        ListFooterComponent={bottomPagination}
+        ListHeaderComponent={headerComponent}
+      />
+    </View>
   );
 
   return toReturn;
