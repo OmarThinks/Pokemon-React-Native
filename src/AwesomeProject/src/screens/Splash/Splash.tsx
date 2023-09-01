6995e197df6fc7efaad6ea256cb15abc2c039a7c
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-09-01 11:55:14 +0300

    used my react native template

---
+++ src/AwesomeProject/src/screens/Splash/Splash.tsx
@@ -0,0 +1,44 @@
+import {initializeLanguage} from '@locale';
+import {RootStackParamList, navigationNames} from '@navigation';
+import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {setTheme} from '@redux';
+import {getStoredTheme} from '@storage';
+import React from 'react';
+import {View} from 'react-native';
+import {useDispatch} from 'react-redux';
+type SplashScreenProps = RouteProp<
+  RootStackParamList,
+  typeof navigationNames.Splash
+>;
+
+const Splash = () => {
+  // Navigation
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+
+  const route = useRoute<SplashScreenProps>();
+  const params = useRoute<SplashScreenProps>().params;
+
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    const logStoredTheme = async () => {
+      const storedTheme = await getStoredTheme();
+      await initializeLanguage();
+      // console.log('storedTheme', storedTheme);
+
+      if (storedTheme) {
+        dispatch(setTheme(storedTheme));
+      }
+
+      navigation.replace(navigationNames.Home);
+    };
+
+    logStoredTheme();
+  }, []);
+
+  return <View />;
+};
+
+export default Splash;
